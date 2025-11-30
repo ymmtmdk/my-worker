804ddd679c6769b521633943d319c532a91f9287
ymmtmdk <ymmtmdk@gmail.com>
2025-11-30 14:55:41 +0900

    m

--- src/index.js
+++ src/index.js
@@ -17,12 +17,6 @@ export default {
     // 観測所ID 46106 のデータを抽出
     const station = data["46106"];
 
-    // 必要な情報だけ返す（例：気温と降水量）
-    const filtered = {
-      temperature: station.temp ? station.temp[0] : null,
-      precipitation: station.precipitation1h ? station.precipitation1h[0] : null
-    };
-
     return new Response(JSON.stringify(station), {
       headers: { "Content-Type": "application/json" }
     });
